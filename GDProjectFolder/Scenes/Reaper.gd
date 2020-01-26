5939e0487f5580860c183c3ae1247c6715c8c56e
Swamsthenerd <Swamsthenerd@users.noreply.github.com>
2020-01-26 11:26:47 +0530

    Animation of Reaper
    lol

---
+++ GDProjectFolder/Scenes/Reaper.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+var speed = 50
+var velocity = Vector2()
+var distance = 0
+
+
+func _ready():
+	velocity.x = speed
+
+func _physics_process(delta):
+	move()
+	animate()
+
+func move():
+	distance += 1
+	if distance == 50:
+		distance = 0
+		velocity.x = -velocity.x
+	velocity = move_and_slide(velocity)
+
+func animate():
+	if velocity.x > 0:
+		$AnimatedSprite.play("Move")
+		$AnimatedSprite.flip_h = false
+	elif velocity.x < 0:
+		$AnimatedSprite.play("Move")
+		$AnimatedSprite.flip_h = true
