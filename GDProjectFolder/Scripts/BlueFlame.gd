268548c313104e88abb359480230afb005ed209c
CrimsonRuby11 <48835057+CrimsonRuby11@users.noreply.github.com>
2020-01-26 12:15:09 +0530

    Update BlueFlame.gd

--- GDProjectFolder/Scripts/BlueFlame.gd
+++ GDProjectFolder/Scripts/BlueFlame.gd
@@ -8,6 +8,7 @@ func _ready():
 
 func _physics_process(delta):
 	animate()
+	dir()
 
 func animate():
 	$AnimatedSprite.play("Idle")
@@ -19,4 +20,9 @@ func _on_BallsTimer_timeout():
 	b.position = position
 	b.dir = b_dir
 	b.rotation = b_dir.angle()
-	
+
+func dir():
+	if Global.playerpos.x > position.x:
+		scale.x = -1
+	else:
+		scale.x = 1
