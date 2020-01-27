4f7630361a7ff0cc047e9886315cba7cd1c14202
Swamsthenerd <Swamsthenerd@users.noreply.github.com>
2020-01-27 10:42:33 +0530

    Updated BlueFlameBalls scripting

--- GDProjectFolder/Scripts/BlueFlameBalls.gd
+++ GDProjectFolder/Scripts/BlueFlameBalls.gd
@@ -2,13 +2,21 @@ extends Area2D
 
 const SPEED = 5
 var dir = Vector2()
+var state = 0
+
+func ready():
+	pass
 
 func _physics_process(delta):
+	state += 1
+	if state == 50: 
+		queue_free()
 	translate(SPEED * dir)
 
 func _on_BlueFlameBalls_body_entered(body):
 	if body.is_in_group("Player"):
 		queue_free()
+
 
 
 
