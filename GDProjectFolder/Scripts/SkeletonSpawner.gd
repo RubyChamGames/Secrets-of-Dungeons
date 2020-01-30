8be2c076a81c30a9ea159d97a9fdc7ab5e0186b6
CrimsonRuby11 <48835057+CrimsonRuby11@users.noreply.github.com>
2020-01-30 19:56:01 +0530

    Skeleton Spawner Mech

---
+++ GDProjectFolder/Scripts/SkeletonSpawner.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+var skele = preload("res://Scenes/Skelebones.tscn")
+
+func _physics_process(delta):
+	if $Timer.is_stopped():
+		if Global.distance(Global.playerpos, position) < 50:
+			$Timer.start()
+
+func _on_Timer_timeout():
+	var s = skele.instance()
+	get_parent().get_parent().get_parent().add_child(s)
+	s.position = Vector2(position.x + 64, position.y)
+	$Timer.stop()
+	
