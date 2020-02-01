4875acdf563c1e01c7718341ac68ac4c04aad19c
CrimsonRuby11 <48835057+CrimsonRuby11@users.noreply.github.com>
2020-02-01 21:00:07 +0530

    Floor 1 startings, and Bug Fixes

--- GDProjectFolder/Scripts/Signboard.gd
+++ GDProjectFolder/Scripts/Signboard.gd
@@ -3,4 +3,6 @@ extends StaticBody2D
 func _physics_process(delta):
 	if Global.distance(Global.playerpos, position) < 50:
 		if Input.is_action_just_pressed("ui_accept"):
-			get_node("Popup").popup_centered_ratio(0.25)
+			get_node("Popup").popup()
+			get_node("Popup").set_global_position(position)
+			
