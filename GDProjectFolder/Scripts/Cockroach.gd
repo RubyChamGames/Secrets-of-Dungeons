e02cc66d611c114e77b59e7c4f5d7607dc5e6811
Swamsthenerd <Swamsthenerd@users.noreply.github.com>
2020-02-01 08:18:58 +0530

    Bug Fixes

--- GDProjectFolder/Scripts/Cockroach.gd
+++ GDProjectFolder/Scripts/Cockroach.gd
@@ -2,14 +2,12 @@ extends KinematicBody2D
 
 var dir
 var speed = 3000
-var velocity = Vector2()
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _physics_process(delta):
 	$AnimatedSprite.flip_h = true
 	dir = (Global.playerpos - position).normalized()
 	look_at(Global.playerpos)
 	move_and_slide(speed * dir * delta)
-	velocity = speed * -dir
